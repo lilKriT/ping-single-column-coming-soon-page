65aa073cc47147fb16a690eccf575af02936f619
Piotr Filipiak <kiddhkane@gmail.com>
2022-05-31 23:35:28 +0200

    Added event listener.

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
+const form = document.querySelector(".notify-me");
+const emailField = document.querySelector("#email");
+
 const emailRegex =
   /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-  
+form.addEventListener('submit', (e) => {
+    alert("Submit");
+});
+
+function isValid(field, regex) {
+  return String(field).toLowerCase().match(regex);
+}
