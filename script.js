f6a9b8fd12e1eb574c08e203377401781c8ba90a
Piotr Filipiak <kiddhkane@gmail.com>
2022-06-01 21:32:48 +0200

    Fixed validation.

--- script.js
+++ script.js
@@ -4,8 +4,15 @@ const emailField = document.querySelector("#email");
 const emailRegex =
   /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-form.addEventListener('submit', (e) => {
-    alert("Submit");
+form.addEventListener("submit", (e) => {
+  let email = emailField.value.trim();
+
+  if (!isValid(email, emailRegex)) {
+    emailField.parentElement.classList.add("invalid");
+    e.preventDefault();
+  } else {
+    emailField.parentElement.classList.remove("invalid");
+  }
 });
 
 function isValid(field, regex) {
